c37a909d4d30288a7d5fd13a78bfc5b506feb3bd
KevinChang <galaxyshapes.0417@gmail.com>
2021-12-10 10:11:31 +0000

    [Finetune]

--- main.js
+++ main.js
@@ -35,40 +35,43 @@ let data = [
   let ulBox = document.querySelector('.ticketCard-area');
   let addButton = document.querySelector('.addTicket-btn');
   let searchBar = document.querySelector('.regionSearch');
-
-  function init(){
-      ulBox.innerHTML = "";
-      data.forEach(function(value,index,array){
-        let htmlTemplate = '<li class="ticketCard"><div class="ticketCard-img"><a href="#"><img src="'+ value.imgUrl +'" alt=""></a><div class="ticketCard-region">'+ value.area +'</div><div class="ticketCard-rank">'+ value.rate +'</div></div><div class="ticketCard-content"><div><h3><a href="#" class="ticketCard-name">'+ value.name +'</a></h3><p class="ticketCard-description">'+ value.description +'</p></div><div class="ticketCard-info"><p class="ticketCard-num"><span><i class="fas fa-exclamation-circle"></i></span>剩下最後 <span id="ticketCard-num"> '+ value.group +' </span> 組</p><p class="ticketCard-price">TWD <span id="ticketCard-price">$'+ value.price +'</span></p></div></div></li>';
-        ulBox.innerHTML += htmlTemplate;
-      })
-  }
+  let form = document.querySelector('.addTicket-form');
 
   function addTicket(){
 
       let error = 0;
+      let needReturn = 0;
+
       let nameValue = document.querySelector('#ticketName').value;
       error = validityInput(nameValue,"#ticketName");
-
+      if(error)
+        needReturn++
       let imgUrlValue = document.querySelector('#ticketImgUrl').value;
       error = validityInput(imgUrlValue,"#ticketImgUrl");
-
+      if(error)
+        needReturn++
       let areaValue = document.querySelector('#ticketRegion').value;
       error = validityInput(areaValue,"#ticketRegion");
-
+      if(error)
+        needReturn++
       let priceValue = document.querySelector('#ticketPrice').value;
       error = validityInput(priceValue,"#ticketPrice");
-
+      if(error)
+        needReturn++
       let ticketNumValue = document.querySelector('#ticketNum').value;
       error = validityInput(ticketNumValue,"#ticketNum");
-
+      if(error)
+        needReturn++
       let ticketRateValue = document.querySelector('#ticketRate').value;
       error = validityInput(ticketRateValue,"#ticketRate");
-
+      if(error)
+        needReturn++
       let ticketDescriptionValue = document.querySelector('#ticketDescription').value;
       error = validityInput(ticketDescriptionValue,"#ticketDescription");
-
-      if( error != 0)
+      if(error)
+        needReturn++
+      
+      if( needReturn != 0)
         return;
 
       let addTicketData = [
@@ -82,63 +85,33 @@ let data = [
         "description": ticketDescriptionValue
       }
       ];
-      data.push(addTicketData[0]);
-      init();
-      clearAllInput();
-  }
-
-  function searchRegion(region){
       
-      let searchRegionArray = [];
-      let searchCount = 0;
+      data.push(addTicketData[0]);
+      renderCards(data,data.length);
+      searchBar.value = "全部地區";
+      form.reset();
+  }
 
-      if(region == "台北"){
-        data.forEach(function(value,index,array){
-            if(value.area == "台北"){
-                searchCount++;
-                searchRegionArray.push(value);
-            }
-        })
-        renderSearch(searchRegionArray,searchCount);
-      }else if(region == "台中"){
-        data.forEach(function(value,index,array){
-            if(value.area == "台中"){
-                searchCount++;
-                searchRegionArray.push(value);
-            }
-        })
-        renderSearch(searchRegionArray,searchCount); 
-      }else if(region == "高雄"){
-        data.forEach(function(value,index,array){
-            if(value.area == "高雄"){
-                searchCount++;
-                searchRegionArray.push(value);
-            }
-        })
-        renderSearch(searchRegionArray,searchCount);  
-      }else if(region == "全部地區"){
-        init();
-        document.querySelector("#searchResult-text").innerHTML = "搜尋資料為" + data.length + "筆";
+  function searchRegion(data,eventTarget){
+
+      let searchInfo = [[], 0] 
+
+      if(eventTarget.value == "全部地區"){
+        searchInfo[0] = data;
+        searchInfo[1] = data.length;
+        return searchInfo;
       }
-  }
-
-  function renderSearch(searchRegionArray,searchCount){
-        ulBox.innerHTML = "";
-        searchRegionArray.forEach(function(value,index,array){
-        let htmlTemplate = '<li class="ticketCard"><div class="ticketCard-img"><a href="#"><img src="'+ value.imgUrl +'" alt=""></a><div class="ticketCard-region">'+ value.area +'</div><div class="ticketCard-rank">'+ value.rate +'</div></div><div class="ticketCard-content"><div><h3><a href="#" class="ticketCard-name">'+ value.name +'</a></h3><p class="ticketCard-description">'+ value.description +'</p></div><div class="ticketCard-info"><p class="ticketCard-num"><span><i class="fas fa-exclamation-circle"></i></span>剩下最後 <span id="ticketCard-num"> '+ value.group +' </span> 組</p><p class="ticketCard-price">TWD <span id="ticketCard-price">$'+ value.price +'</span></p></div></div></li>';
-        ulBox.innerHTML += htmlTemplate;
-        })
-        document.querySelector("#searchResult-text").innerHTML = "搜尋資料為" + searchCount + "筆";
-  }
-
-  function clearAllInput(){
-      let array = document.querySelectorAll('.addTicket-form input,.addTicket-form textarea,.addTicket-form select');
-      array.forEach(function(value,index){
-        if(value.type != "button")
-            value.value = "";
+      
+      searchInfo[0] = data.filter(function(item){
+          if(item.area === eventTarget.value)
+          searchInfo[1]++;
+          return item.area === eventTarget.value; 
       });
-  }
 
+      return searchInfo;
+  }
+  
+  //validate
   function validityInput(inputValue, inputValueElement){
      if(inputValue == ""){
         document.querySelector(inputValueElement +'-message').setAttribute("style","display:block;");
@@ -149,13 +122,29 @@ let data = [
      }
   }
 
+  // Render
+  function renderCards(Array,Count){
+    ulBox.innerHTML = "";
+    Array.forEach(function(value,index,array){
+    let htmlTemplate = '<li class="ticketCard"><div class="ticketCard-img"><a href="#"><img src="'+ value.imgUrl +'" alt=""></a><div class="ticketCard-region">'+ value.area +'</div><div class="ticketCard-rank">'+ value.rate +'</div></div><div class="ticketCard-content"><div><h3><a href="#" class="ticketCard-name">'+ value.name +'</a></h3><p class="ticketCard-description">'+ value.description +'</p></div><div class="ticketCard-info"><p class="ticketCard-num"><span><i class="fas fa-exclamation-circle"></i></span>剩下最後 <span id="ticketCard-num"> '+ value.group +' </span> 組</p><p class="ticketCard-price">TWD <span id="ticketCard-price">$'+ value.price +'</span></p></div></div></li>';
+    ulBox.innerHTML += htmlTemplate;
+    })
+
+    document.querySelector("#searchResult-text").innerHTML = "搜尋資料為" + Count + "筆";
+  }
+
+  //initial
   window.onload=function(){
-    init();
+
+    renderCards(data,data.length);
+
     addButton.addEventListener("click",function(){
         addTicket();
     });
 
     searchBar.addEventListener("change",function(e){
-        searchRegion(e.target.value);
+        let filterSectionData = searchRegion(data,e.target);
+        renderCards(filterSectionData[0],filterSectionData[1]);
     })
+
   }
